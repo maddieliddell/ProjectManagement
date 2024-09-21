05ddcab127beb2ac86d5df1d0a0e185b661e9d91
maddieliddell <madelinecliddell@gmail.com>
2024-09-21 10:52:36 -0400

    taskManager and TitleList code updated

---
+++ project-management-app/src/containers/projects/taskManager/taskManager.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+
+import { newTaskForm } from "../../components/newTaskForm";
+import { TileList } from "../../components/tileList/TileList";
+
+export const taskManager = () => {
+  /*
+  Define state variables for 
+  appointment info
+  */
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    /*
+    Add contact info and clear data  
+    */
+   
+  };
+
+  return (
+    <div>
+      <section>
+        <h2>Add task</h2>
+      </section>
+      <hr />
+      <section>
+        <h2>Taskss</h2>
+      </section>
+    </div>
+  );
+};
