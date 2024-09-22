ed3792657e59e367160ef74da083cfbd723dd989
maddieliddell <madelinecliddell@gmail.com>
2024-09-22 16:13:08 -0400

    added new project form

--- project-management-app/src/containers/dashboard/dashboard.js
+++ project-management-app/src/containers/dashboard/dashboard.js
@@ -1,6 +1,14 @@
 <button onClick={}> Create Task</button>
 <button onClick={}>Create Project</button>
 
+function CreateNewTaskButton () {
+    return <button> New Task</button>;
+}
+
+function CreateNewProjectButton () {
+    return <button> New Project</button>;
+}
+
 function BackButton() {
     return <button>Back To Home</button>;
    }
